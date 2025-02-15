244e9c58578cd31e59cd9aab5ac9f55a532f3e20
athenasadeghi <athenaasadeghi@gmail.com>
2025-02-15 17:41:28 +0330

    vance

---
+++ S01/HW/Exercise2.6/Pythagorean/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Pythagorean;
+
+class Program
+{
+    static void find_pythagorean_triples()
+    {
+        for (int a=1;a<100;a++)
+        {
+            for (int b=a+1;b<100;b++)
+            {
+                for (int c = b+1;c<100;c++)
+                    if (a*a+b*b==c*c)
+                    {
+                       Console.Write(" "+a);
+                       Console.Write(" "+b);
+                       Console.Write(" "+c);
+                       Console.WriteLine();
+                    }
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        find_pythagorean_triples();
+    }
+}
