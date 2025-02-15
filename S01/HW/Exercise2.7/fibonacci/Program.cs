87508a78102cbc589c04d59b8d7f4f0938e38a48
athenasadeghi <athenaasadeghi@gmail.com>
2025-02-15 18:36:56 +0330

    vance

---
+++ S01/HW/Exercise2.7/fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace fibonacci;
+
+class Program
+{
+    static int fibonacci(int n)
+    {
+        if(n<=0)
+          return 0;
+        else if (n==1 | n==2)
+           return 1;
+        else
+        {
+            return fibonacci(n-2) + fibonacci(n-1);
+        }
+    }
+    static void Main(string[] args)
+    {
+        for(int i=1;i<21;i++)
+        {
+            Console.Write(i+":");
+            Console.WriteLine(fibonacci(i));
+        }
+    }
+}
---
+++ S01/HW/Exercise2.7/prime/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace prime;
+
+class Program
+{
+    static bool IsDivisible(int a, int b)
+    {
+        if(a%b==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static bool Is_Prime(int n)
+    {
+        if (n==1)
+        {
+            return false;
+        }
+        else if (n==2)
+        {
+            return true;
+        }
+        else
+        {
+            for (int i=2;i<n;i++)
+            {
+                if (IsDivisible(n,i))
+                   return false;
+            }
+            return true;
+        }
+        
+        
+    }
+
+    static void Main(string[] args)
+    {
+        for(int i=1;i<101;i++)
+        {
+            if (Is_Prime(i))
+               Console.WriteLine(i);
+        }
+    }
+}
